21b3f7d8ea9d1097e684ba8d0bcefa6e7f302a7e
Havir-S <54580940+Havir-S@users.noreply.github.com>
2024-01-03 12:48:39 +0100

    new portfolio uploaded

--- components/en/resume/Resumes.jsx
+++ components/en/resume/Resumes.jsx
@@ -6,20 +6,20 @@ export default function Resumes() {
   return (
     <div className="  col-span-full lg:col-start-2 lg:col-end-9 flex flex-col ">
       <ResumeLink
-        previewImgs={["/portfolio/pl1.jpg", "/portfolio/pl2.jpg"]}
+        previewImgs={["/portfolio/pl1.png", "/portfolio/pl2.png"]}
         downloadLink={"Krzysztof_Szafran_Resume_PL_28-07-2023.pdf"}
         fileName={"Krzysztof Szafran CV PL"}
       />
       <ResumeLink
-        previewImgs={["/portfolio/pl1.jpg", "/portfolio/pl2.jpg"]}
+        previewImgs={["/portfolio/en1.png", "/portfolio/en2.png"]}
         downloadLink={"Krzysztof_Szafran_Resume_EN_28-07-2023.pdf"}
         fileName={"Christopher Saffron CV EN"}
       />
-      <ResumeLink
+      {/* <ResumeLink
         previewImgs={["/portfolio/pl1.jpg", "/portfolio/pl2.jpg"]}
         downloadLink={"Krzysztof_Szafran_Resume_JP_28-07-2023.pdf"}
         fileName={"Christopher Saffron 履歴書 JP"}
-      />
+      /> */}
     </div>
   );
 }
