af895acfb3f5c989512679be80bc4baa2a021151
Havir-S <54580940+Havir-S@users.noreply.github.com>
2024-01-08 17:40:28 +0100

    deleted unnecessary files

--- app/page.js
+++ app/page.js
@@ -7,16 +7,10 @@ import Offer from "@/components/en/home/Offer";
 import Languages from "@/components/en/home/Languages";
 import Media from "@/components/en/home/Media";
 
-import { AnimatePresence, motion, Spring } from "framer-motion";
-import Clouds from "@/components/Clouds";
-import FooterBreak from "@/components/FooterBreak";
-
 export default function Home() {
   return (
-    // <AnimatePresence mode="wait">
     <>
       <main className="main-grid transition  duration-200 shadow-md  bg-white dark:bg-bgcolor  px-3 max-w-fit mx-auto">
-        {/* <main className="main-grid transition duration-200 shadow-md dark:shadow-main bg-white dark:bg-bgcolor px-3 max-w-fit mx-auto"></main> */}
         <Title />
         <NameCard />
         <NameCardImage />
@@ -26,9 +20,6 @@ export default function Home() {
         <Languages />
         <Media />
       </main>
-      <FooterBreak height={6} />
-      {/* <Clouds /> */}
     </>
-    // </AnimatePresence>
   );
 }
--- app/template2.jsx
+++ app/template2.jsx
@@ -6,9 +6,6 @@ const variants = {
   hidden: { opacity: 0 },
   enter: { opacity: 1, transition: { duration: 0.4 } },
   exit: { opacity: 0, transition: { duration: 0.4 } },
-  // hidden: { opacity: 0, x: 0, y: 30 },
-  // enter: { opacity: 1, x: 0, y: 0 },
-  // exit: { opacity: 0, x: 0, y: 100 },
 };
 
 export default function Template({ children }) {
@@ -19,7 +16,6 @@ export default function Template({ children }) {
       <motion.div
         variants={variants}
         initial="hidden"
-        // animate=" "
         animate="enter"
         transition={{ type: "linear" }}
         key={pathName}
