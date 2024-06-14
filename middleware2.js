5e35ec10b06ae3f6680d74625faef97b44db222e
Havir-S <54580940+Havir-S@users.noreply.github.com>
2024-06-14 12:08:31 +0200

    updated 14 06 2024

--- middleware2.js
+++ middleware2.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  console.log("haha");
   return NextResponse.redirect(new URL("/", request.url));
 }
 
